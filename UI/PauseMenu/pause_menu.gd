c38f7d2cb5f0d922b9a8a85556a1af406a1a00e3
Bill-GD <116184478+Bill-GD@users.noreply.github.com>
2024-03-18 19:08:10 +0700

    Pause menu

---
+++ UI/PauseMenu/pause_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+# Pause menu, only control buttons
+# Actual pause/unpause in UI controller (event autoload)
+
+func _on_button_continue_pressed() -> void:
+	# unpause
+	pass
+
+func _on_button_main_menu_pressed() -> void:
+	# back to main menu, discard run
+	pass
